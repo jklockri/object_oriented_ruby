7a28d0c09180b54371c02fc065b6e30be262eaa4
Jay Lockridge <jklockri@pnw.edu>
2017-02-27 19:55:50 -0600

    Initial code

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+# Animal Store 
+animal1={:"animal"=> "Dragon",:"size"=> "Large",:"color"=> "Green", :"price"=> 7}
+animal2={:"animal"=>"Cat",:"size"=>"Huge",:"color"=>"Red", :"price"=>62}
+animal3={:"animal"=>"Fish",:"size"=>"small",:"color"=>"White", :"price"=>50000}
+
+puts "The #{animal1[:animal]} is #{animal1[:size]} and #{animal1[:color]} it costs $#{animal1[:price]}."
+puts "The #{animal2[:animal]} is #{animal2[:size]} and #{animal2[:color]} it costs $#{animal2[:price]}."
+puts "The #{animal3[:animal]} is #{animal3[:size]} and #{animal3[:color]} it costs $#{animal3[:price]}."
+
+
+
