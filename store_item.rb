19802be3ed6e6780b69bc5c907a2868fd5c3f996
Jay Lockridge <jklockri@pnw.edu>
2017-02-27 20:50:39 -0600

    Made class

--- store_item.rb
+++ store_item.rb
@@ -18,3 +18,29 @@ puts "The #{animal2[:animal]} is #{animal2[:size]} and #{animal2[:color]} it cos
 puts "The #{animal3[:animal]} is #{animal3[:size]} and #{animal3[:color]} it costs $#{animal3[:price]}."
 puts 
 
+
+#OOP
+class Store
+  attr_reader :animal,:size,:color,:price
+  attr_writer :price
+
+  def initialize(animal,size,color,price)
+    @animal=animal
+    @size=size
+    @color=color
+    @price=price
+  end
+
+  def print_info
+    puts "The #{@animal} is  #{@size} and #{@color} it costs $#{@price}."
+  end 
+
+end 
+
+animal1=Store.new("Dragon","large","green",7)
+animal2=Store.new("Cat","huge","red",62)
+animal3=Store.new("Fish","small","white",50000)
+
+animal1.print_info
+animal1.price=8
+animal1.print_info
